88533d660379c4ba28b09d66e563a3588f01e0e9
Natan Felles <natanfelles@gmail.com>
2021-09-02 21:03:24 -0300

    Allow run aplus from composer global installation

--- src/aplus.php
+++ src/aplus.php
@@ -1,6 +1,10 @@
 #!/usr/bin/env php
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+if (is_file(__DIR__ . '/../../../autoload.php')) {
+    require __DIR__ . '/../../../autoload.php';
+} else {
+    require __DIR__ . '/../vendor/autoload.php';
+}
 
 use Aplus\Commands\Index;
 use Framework\CLI\Console;
