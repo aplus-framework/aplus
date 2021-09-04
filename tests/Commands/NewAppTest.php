d9d302af971e710aedcbe5aa1ebf3080edbaf1ff
Natan Felles <natanfelles@gmail.com>
2021-09-04 18:10:28 -0300

    Test new-app

---
+++ tests/Commands/NewAppTest.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * This file is part of Aplus.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Commands;
+
+use Aplus\Commands\NewApp;
+use Framework\CLI\Streams\Stdout;
+
+/**
+ * Class NewAppTest.
+ */
+final class NewAppTest extends TestCase
+{
+    protected string $command = NewApp::class;
+
+    public function testNewApp() : void
+    {
+        $dir = \sys_get_temp_dir() . '/aplus-app';
+        if (\is_dir($dir)) {
+            \rmdir($dir);
+        }
+        Stdout::init();
+        $this->console->exec('new-app ' . $dir);
+        self::assertStringContainsString(
+            'App Project structure created at "' . $dir . '"',
+            Stdout::getContents()
+        );
+    }
+}
