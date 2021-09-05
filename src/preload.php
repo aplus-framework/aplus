d69f5c3488e7bbc434a2b6ee2a7fea8aaf6cda6c
Natan Felles <natanfelles@gmail.com>
2021-09-05 13:58:22 -0300

    Require Preloader on global installations

--- src/preload.php
+++ src/preload.php
@@ -7,7 +7,11 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-require __DIR__ . '/../vendor/aplus/autoload/src/Preloader.php';
+if (is_file(__DIR__ . '/../../autoload/src/Preloader.php')) {
+    require __DIR__ . '/../../autoload/src/Preloader.php';
+} else {
+    require __DIR__ . '/../vendor/aplus/autoload/src/Preloader.php';
+}
 
 use Framework\Autoload\Preloader;
 
